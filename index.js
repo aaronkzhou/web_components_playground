869895d80cf57864f94865234a300646f0803d8e
Aaron Zhou <aaaron.chou@gmail.com>
2019-06-11 22:49:19 +1200

    Add basic stuff in

--- index.js
+++ index.js
@@ -8,5 +8,19 @@ window.addEventListener('load', () => {
 async function fetchMovies() {
   const res = await fetch(API_URL);
   const data = await res.json();
+  const main = document.querySelector('main');
+  data._embedded.episodes.forEach(episode => {
+    const el = document.createElement('movie-grid');
+    el.episode = episode;
+    main.appendChild(el);
+  });
   console.log(data);
 }
+
+class MovieGrid extends HTMLElement {
+  set episode(episode) {
+    this.innerHTML = `${episode}`;
+  }
+}
+
+customElements.define('movie-grid', MovieGrid);
